c8de8547e2d6609b154415381339ebe77f19bffb
zhrk <zshchipets@gmail.com>
2026-08-18 14:05:33 +0400

    noforwards media

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
-import { TelegramClient } from 'telegram';
+import { Api, TelegramClient } from 'telegram';
 import { StringSession } from 'telegram/sessions';
 import { Logger } from 'telegram/extensions/Logger';
 import config from '../config.json';
-import { writeFileSync, mkdirSync } from 'fs';
+import { mkdirSync } from 'fs';
 import path from 'path';
 import { LogLevel } from 'telegram/extensions/Logger';
 import { createInterface } from 'readline/promises';
 import { NewMessage } from 'telegram/events';
-
-const chats = Object.values(config.channels_ids);
 
 const rl = createInterface({ input: process.stdin, output: process.stdout });
 
@@ -25,12 +23,57 @@ const rl = createInterface({ input: process.stdin, output: process.stdout });
     onError: (error) => console.log(error),
   });
 
-  mkdirSync(path.join(process.cwd(), 'output'), { recursive: true });
+  const dialogs = await client.getDialogs();
+
+  const noForwardsDialogs = dialogs.filter((dialog) => {
+    const entity = dialog.entity;
+
+    if (!entity) return false;
+    if (entity instanceof Api.Channel) return entity.noforwards === true;
+    if (entity instanceof Api.Chat) return entity.noforwards === true;
+
+    return false;
+  });
+
+  const chats = noForwardsDialogs.map((item) => item.id?.toString() || '');
+
+  const outputDir = path.join(process.cwd(), 'output');
+  mkdirSync(outputDir, { recursive: true });
 
   client.addEventHandler(async (event) => {
-    writeFileSync(
-      path.join(process.cwd(), 'output', `${new Date().getTime()}.json`),
-      JSON.stringify(event.message, null, 2)
-    );
+    const message = event.message;
+
+    if (message.media) {
+      let userId = 'unknown';
+
+      if (message.peerId) {
+        if ('userId' in message.peerId) {
+          userId = message.peerId.userId.toString();
+        } else if ('channelId' in message.peerId) {
+          userId = message.peerId.channelId.toString();
+        } else if ('chatId' in message.peerId) {
+          userId = message.peerId.chatId.toString();
+        }
+      }
+
+      const mappedId = Object.entries(config.ids)
+        .find(([id]) => id === userId)
+        ?.at(1);
+
+      const folder = mappedId || userId;
+
+      const userDir = path.join(outputDir, folder);
+      mkdirSync(userDir, { recursive: true });
+
+      try {
+        await client.downloadMedia(message.media, { outputFile: userDir });
+
+        if (!mappedId) {
+          console.log(`${message.id} -> ${folder}`);
+        }
+      } catch (error) {
+        console.error(`[Media][${message.id}]:`, error);
+      }
+    }
   }, new NewMessage({ chats }));
 })();
